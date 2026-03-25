8acf6eb4e8b51824c019df7e0682698aaf3a70d1
ponzaa567 <ponpon13173@gmail.com>
2026-03-25 23:10:12 +0700

    - init

---
+++ src/components/Portal.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Portal '
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Rules.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Rules'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
